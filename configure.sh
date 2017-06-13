56d1b0cde6f3af33c5b84dc755aa30b7f08992dc
TropicalBastos <ian-bastos@live.com>
2017-06-13 01:11:02 +0100

    Added install script

--- configure.sh
+++ configure.sh
@@ -1,4 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo g++ src/main.cpp src/invert.cpp -o bin/invert.out \
- -std=c++11 -lX11 -lpthread
+
+
+echo "Compiling source..."
+
+g++ src/main.cpp src/invert.cpp -o bin/invertimage.out \
+-std=c++11 -lX11 -lpthread &
+
+#grab most recent process and store it in pid
+pid=$!
+
+trap "kill $pid 2> /dev/null" EXIT
+
+chars="/-\|"
+limit=20
+
+for (( i=0; i<$limit; i++ )); do
+  sleep 0.1
+  echo -en "."
+done
+
+while kill -0 $pid 2> /dev/null; do
+ for (( i=0; i<${#chars}; i++ )); do
+   sleep 0.3
+   #show char at position $i for only 1 len
+   echo -en "\r" "${chars:$i:1}"
+ done
+done
+
+echo -e "\n"
+
+echo "Compile complete!"
+echo "Installing binaries to path"
+
+path="/usr/bin/"
+
+chmod 777 invertimg
+cp bin/invertimage.out $path
+cp invertimg $path
+
+echo "Install complete!"
